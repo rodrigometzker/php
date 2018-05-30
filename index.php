1c3c6580d4e9eb785851b6a4fa8cb1a5f7c46985
Rodrigo <rodrigometzker10@gmail.com>
2018-05-29 23:54:59 -0300

    resolvi o conflito de olás (correto)

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 
-<<<<<<< HEAD
-echo "Olá mundo! 456"
+<?php
+     
+      echo "Olá mundo! 456";
 
-echo "Olá udemy"
-=======
-     echo "Olá mundo! 123";
-  
-     echo "Olá HCode";
->>>>>>> e2e8508ec843e0b5b0ae8c6387390c53c97d6320
+      echo "Olá udemy";
 
 ?>
